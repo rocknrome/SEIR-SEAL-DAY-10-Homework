916faa8c28f96edd93db158e98b863ed4eca0a9c
Roman Larionov <pcsaveddotcom@gmail.com>
2023-11-22 12:28:36 -0500

    Commit 1 - I added my first element and it was fun

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 // if (typeof jQuery == 'undefined'){
 //     console.log('oops! I still have to link my jQuery properly!');
 //   } else {console.log('I did it! I linked jQuery and this js file!')};
-  
+
+$(() => {
+	//query for div witd id "container"
+    const $container = $('container');
+    console.log($container);
+
+    //creating the h1 element
+    const $h1 = $('<h1>').text('Hogwarts');
+    console.log($h1);
+
+    //appending $h1 to $container
+    $container.append($h1);                 //it has been created in JS and not yet added to DOM
+});                                         //therefore it is not visible yet on the webpage
