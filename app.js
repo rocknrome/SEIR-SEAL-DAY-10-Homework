2064195d9b7f67b3d0bc9fb88173cebe5f3d10a4
Roman Larionov <pcsaveddotcom@gmail.com>
2023-11-22 20:59:44 -0500

    Commit 2 - I added more elements. My power grows with my dedication and hard work

--- app.js
+++ app.js
@@ -4,13 +4,31 @@
 
 $(() => {
 	//query for div witd id "container"
-    const $container = $('container');
-    console.log($container);
+    const $container = $('#container');
+        console.log($container);
 
     //creating the h1 element
     const $h1 = $('<h1>').text('Hogwarts');
-    console.log($h1);
+        console.log($h1);
 
     //appending $h1 to $container
     $container.append($h1);                 //it has been created in JS and not yet added to DOM
-});                                         //therefore it is not visible yet on the webpage
+                                         //therefore it is not visible yet on the webpage
+    //h2 element
+    const $h2 = $('<h2>').text('RockNRome');
+        $container.append($h2);
+
+    //h3 element
+    const $h3 = $('<h3>').text('Gryffindor');
+        $container.append($h3);
+
+    //h4 element "Pet"
+    const $h4Pet = $('<h4>').text('Cookie').addClass('cat');
+        $container.append($h4Pet);
+
+    //h4 element "Wand"
+    const $h4Wand = $('<h4>').text('Holly Wand with Unicorn Hair Core');
+        $container.append($h4Wand);
+
+
+});
